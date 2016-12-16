27020a2c65faffcd599e13033256959563aba4e9
Ferg Smyth <fergsmyth@gmail.com>
2016-12-16 00:30:45 +0000

    Fixing png name of test file

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var im = require('imagemagick');
-im.identify('kittens.jpg', function(err, features){
+im.identify('1.png', function(err, features){
   if (err) throw err;
     console.log(features);
       // { format: 'JPEG', width: 3904, height: 2622, depth: 8 }
