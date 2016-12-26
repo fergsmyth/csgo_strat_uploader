e89544da70d7ba51fb4ca54d3927b4d48ad92301
Ferg Smyth <fergsmyth@gmail.com>
2016-12-26 18:23:38 +0000

    Renamed main file to convert.js

---
+++ convert.js
@@ -0,0 +1,21 @@
+const gm = require('gm');
+const fs = require('fs');
+const path = require('path');
+
+const inputs = './inputs/';
+const outputs = './outputs/';
+
+fs.readdir(inputs, (err, files) => {
+		files.forEach(file => {
+				var fileName = path.basename(file, '.tga');
+				gm(inputs + file)
+				.write(outputs + fileName + '.png', function (err) {
+						if (err){ 
+						console.log(err);
+						} else {
+						console.log(fileName);	
+						}
+						});
+				});
+		});
+
