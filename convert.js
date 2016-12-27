7dc682910b1eff0dde028c60fbd1d81f8e6e9bfe
Ferg Smyth <fergsmyth@gmail.com>
2016-12-27 00:26:24 +0000

    Added logging to file using Wwinston.

--- convert.js
+++ convert.js
@@ -1,25 +1,40 @@
 const gm = require('gm');
 const fs = require('fs');
 const path = require('path');
+const winston = require('winston');
+const GIFEncoder = require('gifencoder');
 
 const inputs = './inputs/';
 const outputs = './outputs/';
+const log = './logs/conversion.log';
+winston.add(winston.transports.File, { filename: log });
 
 if (!fs.existsSync(outputs)){
 	fs.mkdirSync(outputs);
 }
 
-fs.readdir(inputs, (err, files) => {
-		files.forEach(file => {
-				var fileName = path.basename(file, '.tga');
-				gm(inputs + file)
-				.write(outputs + fileName + '.png', function (err) {
-						if (err){ 
-						console.log(err);
-						} else {
-						console.log(fileName);	
-						}
-						});
+const inputFiles = fs.readdirSync(inputs);
+
+inputFiles.forEach(file => {
+		winston.log("info", "Converting " + file + " to png.");
+		var fileName = path.basename(file, '.tga');
+		gm(inputs + file)
+		.write(outputs + fileName + '.png', function (err) {
+				if (err){	
+				winston.log('error', fileName + " conversion failed");
+				winston.log('error', err);	
+				} 
+				else {winston.log('info', fileName + " conversion complete."); }
 				});
 		});
 
+const encoder = new GIFEncoder();
+encoder.setRepeat(0);
+encoder.setDelay(1200);
+encoder.start();
+
+fs.readdir(outputs, (err, files) => {
+		files.forEach(file => {
+
+				});
+		});
